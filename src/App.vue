93a066f7782b480104b0ac5b3159c75b06a51a79
anlor <andreaslorentsson99@gmail.com>
2019-08-30 19:00:54 +0200

    added headline to list of messages

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <Logo class="Header-logo" />
     </header>
     <div class="MessageList-wrapper">
+      <h1 class="MessageList-headline">Messages</h1>
       <MessageList delay="2000" />
     </div>
     <ChatModal />
@@ -29,26 +30,36 @@ export default {
 <style lang="stylus">
 
 @require 'styles/global/reset'
+@require 'styles/global/colors'
 
 #app
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding: 0 15px
 
   *
     border-box: border-box
 
 .Header
-  padding: 30px 15px
+  padding: 30px
+  border-bottom: solid 1px $border-color
 
 .Header-logo
   width: 160px
 
 .MessageList-wrapper
-  width: 50%;
+  width: 50%
+  margin-top: 30px
+  padding: 0 15px
 
   @media screen and (max-width: 640px)
     width: 100%
+
+.MessageList-headline
+  font-size: 24px
+  font-weight: bold
+  padding: 0 15px
+  margin-bottom: 15px
+  color: $blue
 
 </style>
